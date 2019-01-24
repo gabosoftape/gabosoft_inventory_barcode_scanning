5e615fd5198aa883b1cfe2d59f8dd206182475b5
gabriel pabon <gabosoft.ape@gmail.com>
2019-01-24 12:06:29 -0500

    recode

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,49 +4,33 @@ from odoo import fields, models, api
 from odoo.exceptions import Warning, UserError, AccessError, ValidationError
 
 
-#class StockPicking(models.Model):
-#    _inherit = 'stock.picking'
-
-#    barcode = fields.Char(string='Barcode')
-
-#    @api.onchange('barcode')
-#    def barcode_scanning(self):
-#        match = False
-#        product_obj = self.env['product.product']
-#        product_id = product_obj.search([('barcode', '=', self.barcode)])
-#        if self.barcode and not product_id:
-#            self.barcode = None
-#            raise Warning('Ningun producto coincide con el codigo escaneado')
-#        if self.barcode and self.move_lines:
-#            for line in self.move_lines:
-#                if line.product_id.barcode == self.barcode:
-#                    line.quantity_done += 1
-#                    self.barcode = None
-#                    match = True
-#        if self.barcode and not match:
-#            self.barcode = None
-#            if product_id:
-#                raise Warning('este producto no esta disponible en la orden'
-#                              'Puedes agregar este producto en "add product" y escaneas nuevamente')
-
-
-#class StockPickingOperation(models.Model):
-#    _inherit = 'stock.move'
-
-#    barcode = fields.Char(string='Barcode')
-
-#    @api.onchange('barcode')
-#    def _onchange_barcode_scan(self):
-#        product_rec = self.env['product.product']
-#        if self.barcode:
-#            product = product_rec.search([('barcode', '=', self.barcode)])
-#            self.product_id = product.id
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
-
-class StockPickingBarCode(models.Model):
+class StockPicking(models.Model):
     _inherit = 'stock.picking'
+
+    barcode = fields.Char(string='Barcode')
+    temp_barcode = fields.Char("Barcode lectura")
+    productcodes_ids = fields.One2many('list.productcode', 'picking_id', string='Productos')
+    picking_checked = fields.Boolean("Ready Picking", compute="_get_picking_checked")
+
+    @api.onchange('barcode')
+    def barcode_scanning(self):
+        match = False
+        product_obj = self.env['product.product']
+        product_id = product_obj.search([('barcode', '=', self.barcode)])
+        if self.barcode and not product_id:
+            self.barcode = None
+            raise Warning('Ningun producto coincide con el codigo escaneado')
+        if self.barcode and self.move_lines:
+            for line in self.move_lines:
+                if line.product_id.barcode == self.barcode:
+                    line.quantity_done += 1
+                    self.barcode = None
+                    match = True
+        if self.barcode and not match:
+            self.barcode = None
+            if product_id:
+                raise Warning('este producto no esta disponible en la orden'
+                              'Puedes agregar este producto en "add product" y escaneas nuevamente')
 
     @api.multi
     @api.depends('productcodes_ids.bool_barcode','productcodes_ids.qty')
@@ -58,18 +42,16 @@ class StockPickingBarCode(models.Model):
                 if move_products == products:
                     picking.picking_checked = True
 
-    temp_barcode = fields.Char("Barcode")
-    productcodes_ids = fields.One2many('list.productcode', 'picking_id', string='Productos')
-    picking_checked = fields.Boolean("Ready Picking", compute="_get_picking_checked")
+
 
     @api.onchange('temp_barcode')
     def onchange_temp_barcode(self):
         res = {}
-        barcode = self.temp_barcode
-        if barcode:
+        barcode_aux = self.temp_barcode
+        if barcode_aux:
             new_lines = self.env['list.productcode']
             for move in self.move_lines:
-                if move.product_id.barcode == barcode:
+                if move.product_id.barcode == barcode_aux:
                     pcode = self.productcodes_ids.filtered(lambda r: r.product_id.id == move.product_id.id)
                     if pcode:
                         pcode.qty += 1.0
@@ -87,6 +69,21 @@ class StockPickingBarCode(models.Model):
                         new_lines += new_line
             self.productcodes_ids += new_lines
             self.temp_barcode = ""
+
+class StockPickingOperation(models.Model):
+    _inherit = 'stock.move'
+
+    barcode = fields.Char(string='Barcode')
+
+    @api.onchange('barcode')
+    def _onchange_barcode_scan(self):
+        product_rec = self.env['product.product']
+        if self.barcode:
+            product = product_rec.search([('barcode', '=', self.barcode)])
+            self.product_id = product.id
+
+
+
 
 class ListProductcode(models.Model):
     _name = 'list.productcode'
