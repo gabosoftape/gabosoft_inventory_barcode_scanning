facd03aa0108c286feb6d5d7c1aa0ef4ea2904c1
gabriel pabon <gabosoft.ape@gmail.com>
2019-01-28 23:41:31 -0500

    .

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -5,43 +5,7 @@ from odoo.exceptions import Warning
 from time import sleep
 
 
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
 
-    barcode = fields.Char(string='Barcode')
-
-    @api.onchange('barcode')
-    def barcode_scanning(self):
-        match = False
-        product_obj = self.env['product.product']
-        product_id = product_obj.search([('barcode', '=', self.barcode)])
-        if self.barcode and not product_id:
-            self.barcode = None
-            raise Warning('Ningun producto coincide con el codigo escaneado')
-        if self.barcode and self.move_lines:
-            for line in self.move_lines:
-                if line.product_id.barcode == self.barcode:
-                    line.quantity_done += 1
-                    self.barcode = None
-                    match = True
-        if self.barcode and not match:
-            self.barcode = None
-            if product_id:
-                raise Warning('este producto no esta disponible en la orden'
-                              'Puedes agregar este producto en "add product" y escaneas nuevamente')
-
-
-class StockPickingOperation(models.Model):
-    _inherit = 'stock.move'
-
-    barcode = fields.Char(string='Barcode')
-
-    @api.onchange('barcode')
-    def _onchange_barcode_scan(self):
-        product_rec = self.env['product.product']
-        if self.barcode:
-            product = product_rec.search([('barcode', '=', self.barcode)])
-            self.product_id = product.id
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
 
@@ -103,28 +67,6 @@ class StockPickingBarCode(models.Model):
             self.productcodes_ids += new_lines
             self.move_lines += real_lines
             self.temp_barcode = ""
-
-
-
-
-        # for cargado
-        #    for move in self.move_lines:
-        #        if move.product_id.barcode == barcode:
-        #            pcode = self.productcodes_ids.filtered(lambda r: r.product_id.id == move.product_id.id)
-        #            if pcode:
-        #                pcode.qty += 1.0
-        #                if pcode.qty > move.product_uom_qty:
-        #                    warning = {
-        #                        'title': _('Warning!'),
-        #                        'message': _('The quantity checked is bigger than quantity in picking move for product %s.'%move.product_id.name),
-        #                    }
-        #                    return {'warning': warning}
-        #            else:
-        #                new_line = new_lines.new({
-        #                    'product_id': move.product_id.id,
-        #                    'qty': 1,
-        #                })
-        #                new_lines += new_line
 
 
 class ListProductcode(models.Model):
