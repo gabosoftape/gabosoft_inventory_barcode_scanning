0da49f7a853477241ed1171e5ca07383e856fa92
gabriel pabon <gabosoft.ape@gmail.com>
2019-01-29 14:47:06 -0500

    update test log and structure code

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -25,6 +25,12 @@ class StockPickingOperation(models.Model):
 class StockPickingBarCode(models.Model):
     _inherit = 'stock.picking'
 
+    barcode = fields.Char(string='Barcode')
+    temp_barcode = fields.Char("Barcode Tempo", required=False)
+    productcodes_ids = fields.One2many('list.productcode', 'picking_id', string='Productos')
+    picking_checked = fields.Boolean("Ready Picking", compute="_get_picking_checked")
+    log_scanner = fields.Char("log escaner", readonly=True)
+
     @api.multi
     @api.depends('productcodes_ids.bool_barcode','productcodes_ids.qty')
     def _get_picking_checked(self):
@@ -34,12 +40,6 @@ class StockPickingBarCode(models.Model):
                 products = picking.productcodes_ids.mapped('product_id')
                 if move_products == products:
                     picking.picking_checked = True
-
-    barcode = fields.Char(string='Barcode')
-    temp_barcode = fields.Char("Barcode Tempo", required=False)
-    productcodes_ids = fields.One2many('list.productcode', 'picking_id', string='Productos')
-    picking_checked = fields.Boolean("Ready Picking", compute="_get_picking_checked")
-    log_scanner = fields.Char("log escaner", readonly=True)
 
 #        if self.barcode and self.move_lines:
 #            for line in self.move_lines:
@@ -90,16 +90,16 @@ class StockPickingBarCode(models.Model):
 class ListProductcode(models.Model):
     _name = 'list.productcode'
 
-    @api.multi
-    @api.depends('qty')
-    def _get_bool_barcode(self):
-        for record in self:
-            move = record.picking_id.move_lines.filtered(lambda r: r.product_id.id == record.product_id.id)
-            record.bool_barcode = record.qty == move.product_uom_qty and True or False
-
     barcode = fields.Char('Codigo de Barras', related='product_id.barcode')
     default_code = fields.Char('Referencia', related='product_id.default_code')
     product_id = fields.Many2one('product.product', string='Producto')
     qty = fields.Float("Cantidad", default=1)
     picking_id = fields.Many2one('stock.picking', "Picking", ondelete='cascade')
     bool_barcode = fields.Boolean("Barcode Checked", compute="_get_bool_barcode")
+
+    @api.multi
+    @api.depends('qty')
+    def _get_bool_barcode(self):
+        for record in self:
+            move = record.picking_id.move_lines.filtered(lambda r: r.product_id.id == record.product_id.id)
+            record.bool_barcode = record.qty == move.product_uom_qty and True or False
