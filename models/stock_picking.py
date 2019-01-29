ec74342801ae0ee0a7c59524ab10fd76f0ab10dd
gabriel pabon <gabosoft.ape@gmail.com>
2019-01-28 21:56:58 -0500

    ..,

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -82,6 +82,7 @@ class StockPickingBarCode(models.Model):
                 for line in self.productcodes_ids:
                     if line.product_id.barcode == barcode:
                         line.qty += 1
+                        self.temp_barcode = ""
                     else:
                         new_line = new_lines.new({
                             'product_id': product_id.id,
