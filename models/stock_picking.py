43f9ee91d0e006b40ce20d811e84c82b02506c09
gabriel pabon <gabosoft.ape@gmail.com>
2019-01-28 23:02:24 -0500

    test remove last classes

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -4,47 +4,6 @@ from odoo import fields, models, api
 from odoo.exceptions import Warning
 from time import sleep
 
-
-class StockPicking(models.Model):
-    _inherit = 'stock.picking'
-
-    barcode = fields.Char(string='Barcode')
-
-    @api.onchange('barcode')
-    def barcode_scanning(self):
-        match = False
-        product_obj = self.env['product.product']
-        product_id = product_obj.search([('barcode', '=', self.barcode)])
-        if self.barcode and not product_id:
-            self.barcode = None
-            raise Warning('Ningun producto coincide con el codigo escaneado')
-        if self.barcode and self.move_lines:
-            for line in self.move_lines:
-                if line.product_id.barcode == self.barcode:
-                    line.quantity_done += 1
-                    self.barcode = None
-                    match = True
-        if self.barcode and not match:
-            self.barcode = None
-            if product_id:
-                raise Warning('este producto no esta disponible en la orden'
-                              'Puedes agregar este producto en "add product" y escaneas nuevamente')
-
-
-class StockPickingOperation(models.Model):
-    _inherit = 'stock.move'
-
-    barcode = fields.Char(string='Barcode')
-
-    @api.onchange('barcode')
-    def _onchange_barcode_scan(self):
-        product_rec = self.env['product.product']
-        if self.barcode:
-            product = product_rec.search([('barcode', '=', self.barcode)])
-            self.product_id = product.id
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-
 class StockPickingBarCode(models.Model):
     _inherit = 'stock.picking'
 
@@ -58,7 +17,7 @@ class StockPickingBarCode(models.Model):
                 if move_products == products:
                     picking.picking_checked = True
 
-    temp_barcode = fields.Char("Barcode")
+    temp_barcode = fields.Char("Barcode Temporal")
     productcodes_ids = fields.One2many('list.productcode', 'picking_id', string='Productos')
     picking_checked = fields.Boolean("Ready Picking", compute="_get_picking_checked")
     log_scanner = fields.Char("log escaner", readonly=True)
@@ -90,7 +49,7 @@ class StockPickingBarCode(models.Model):
                         'product_id': product_id.id,
                         'qty': 1,
                     })
-                    new_lines += new_line                
+                    new_lines += new_line
 
             else:
                 self.log_scanner = "Guardar primer elemento"
