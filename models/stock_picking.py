9c4a3ca5be18245345225fdf7015b8724bacdbae
gabriel pabon <gabosoft.ape@gmail.com>
2019-02-07 16:52:23 -0500

    ....

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -206,7 +206,9 @@ class StockPickingBarCode(models.Model):
             picking_obj += new_line
             picking_obj._action_confirm()
             picking_obj._action_assign()
+            picking_obj.move_line_ids.write({'qty_done': line.qty})
             self.move_lines += picking_obj
+            picking_obj._action_done()
         self.log_scanner = "se guardaron los movimientos ok"
         return self.move_lines
 
