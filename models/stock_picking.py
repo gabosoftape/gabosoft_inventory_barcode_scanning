cc3b8bb8543b2c76c0566f58651fffd688d7bbe0
gabriel pabon <gabosoft.ape@gmail.com>
2019-02-07 02:26:01 -0500

    update

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -186,7 +186,6 @@ class StockPickingBarCode(models.Model):
                     'product_id': product_id.id,
                     'quantity_done': 1,
                     'product_uom': 1,
-                    'product_uom_qty': 1,
                     'state': 'done',
                 })
                 real_lines += real_line
