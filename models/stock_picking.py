bca3b30a5c5a9b98def8e47b378ab9d4c4dd7d1b
gabriel pabon <gabosoft.ape@gmail.com>
2019-02-12 12:00:26 -0500

    test add value from new line ... named product_qty

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -200,6 +200,7 @@ class StockPickingBarCode(models.Model):
                 'quantity_done': line.qty,
                 'product_id': line.product_id.id,
                 'product_uom': 1,
+                'product_qty': line.qty,
                 'location_id': location.id,
                 'location_dest_id': location_dest.id,
                 })
