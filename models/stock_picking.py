5f8defb053b21d975751e275ac42180559aa9062
gabriel pabon <gabosoft.ape@gmail.com>
2019-02-13 11:43:42 -0500

    test adding fields to view inventory ajustments

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -250,3 +250,66 @@ class productProduct(models.Model):
 
     default_talla = fields.Char('Talla')
     default_color = fields.Char('Color')
+
+class automatedInventory(models.Model):
+    _inherit = 'stock.inventory'
+
+    inventory_barcode = fields.Char("Barcode Inventario", required=False)
+    talla = fields.Char('Talla', related='product_id.default_talla')
+    color = fields.Char('Color', related='product_id.default_color')
+    log_scanner = fields.Char("log escaner", readonly=True)
+
+    @api.onchange('inventory_barcode')
+    def onchange_inventory_barcode(self):
+        self.log_scanner = ""
+        flag = False
+        barcode = self.inventory_barcode
+        location = self.location_id
+        filtro = self.filter
+        product_rec = self.env['product.product']
+        product_id = product_rec.search([('barcode', '=', barcode)])
+        if barcode and not product_id:
+            self.log_scanner = "Elemento desconocido!!!!!!!!"
+            self.inventory_barcode = ""
+        if barcode and product_id:
+            new_lines = self.env['stock.inventory.line']
+            picking_lines = {}
+            real_lines = self.env['stock.move']
+            size = len(self.line_ids)
+            if barcode and size > 0:
+                for line in self.line_ids:
+                    if line.product_id.barcode == barcode:
+                        line.product_qty += 1
+                        self.inventory_barcode = ""
+                if self.inventory_barcode == "":
+                        self.log_scanner = "se agrego cantidad"
+                else:
+                    self.log_scanner = "Como no coincide con ningun elemento de la lista, agregamos nuevo "
+                    new_line = new_lines.create({
+                        'product_id': product_id.id,
+                        'location_id': location,
+                        'product_qty': 1,
+                    })
+                    new_lines += new_line
+            else:
+                #elemento nuevo en la lista
+                self.log_scanner = "se creó primer elemento"
+                new_line = new_lines.create({
+                    'product_id': product_id.id,
+                    'location_id': location,
+                    'product_qty': 1,
+                })
+                new_lines += new_line
+#no borrar , para efectos del test necesito comentar este pedazo.
+            #    real_line = real_lines.new({
+            #        'name': self.name,
+            #        'product_id': product_id.id,
+            #        'quantity_done': 1,
+            #        'product_uom': 1,
+            #        'date_expected': self.scheduled_date,
+            #    })
+            #    real_lines += real_line
+
+            self.line_ids += new_lines
+            #self.move_lines += real_lines
+            self.temp_barcode = ""
